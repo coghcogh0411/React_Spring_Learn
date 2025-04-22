d409703e07c988ea4e8bdb7c73136059bc93e759
coghcogh0411 <coghcogh0411@naver.com>
2025-04-23 03:45:59 +0900

    react

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login.js';
+import Signup from './pages/Signup.js';
+import Board from './pages/Board.js';
+import PostDetail from './pages/PostDetail.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Login />}/>
+      <Route path='/signup' element={<Signup />}/>
+      <Route path='/board' element={<Board />}/>
+      <Route path='/postDetail' element={<PostDetail />}/>
+    </Routes>
   );
 }
 
